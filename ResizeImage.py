89787a64bad16b7e76ed482fa719b54254a7b22b
Tom Chapman <107841862+chapmantom055@users.noreply.github.com>
2023-12-19 18:31:40 +0000

    Add files via upload

--- ResizeImage.py
+++ ResizeImage.py
@@ -1,11 +1,9 @@
+import traceback
 import boto3
-import os
-import sys
-import pathlib
 from io import BytesIO
-from PIL import Image
+from PIL import Image, ImageOps, ExifTags
 from botocore.exceptions import ClientError
-import PIL.Image
+
 
 s3 = boto3.resource('s3')
 
@@ -26,11 +24,43 @@ def resize_image(src_bucket, key, des_bucket):
         else:
             print("Unexpected error: %s" % e)
 
+# Method 1:
+    # try:
+    #     img = Image.open(BytesIO(file_byte))
+    #     format = img.format
+    #     if hasattr(img, '_getexif'): # only present in JPEGs
+    #         for orientation in ExifTags.TAGS.keys(): 
+    #             if ExifTags.TAGS[orientation]=='Orientation':
+    #                 break 
+    #         e = img._getexif()       # returns None if no EXIF data
+    #         if e is not None:
+    #             exif=dict(e.items())
+    #             orientation = exif[orientation] 
 
-    img = Image.open(BytesIO(file_byte))
-    img.thumbnail(size, Image.LANCZOS)
-    img.save(file, format=img.format, optimize=True)
-    content = file.getvalue()
+    #             if orientation == 3:   img = img.transpose(Image.ROTATE_180)
+    #             elif orientation == 6: img = img.transpose(Image.ROTATE_270)
+    #             elif orientation == 8: img = img.transpose(Image.ROTATE_90)
+
+    #     img.thumbnail(size, Image.LANCZOS)
+    #     img.save(file, format=format, optimize=True)
+    #     content = file.getvalue()
+
+    # except:
+    #     traceback.print_exc()
+
+
+# Method 2:
+    try :
+        img = Image.open(BytesIO(file_byte))
+        if img.format in ('jpg','jpeg', 'JPG', 'JPEG'):
+            ImageOps.exif_transpose(img, in_place=True)
+
+        img.thumbnail(size, Image.LANCZOS)
+        img.save(file, format=img.format, optimize=True)
+        content = file.getvalue()
+
+    except:
+        traceback.print_exc()
     
     try:
         response = client.put_object(
@@ -48,12 +78,17 @@ def resize_image(src_bucket, key, des_bucket):
         else:
             print("Unexpected error: %s" % e)
 
+
+    file.close()
+
     
 
 def lambda_handler(event, context):
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
+
+        print(f'Hold tight, resizing {bucket}/{key}...')
     
         resize_image(src_bucket=bucket, key=key, des_bucket=destBucket)
 
